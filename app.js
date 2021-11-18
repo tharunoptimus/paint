554ad03f7ed9230b2511acacfae9c05c39c2948c
tharunoptimus <tharunoptimus@outlook.com>
2021-11-18 14:50:53 +0530

    Started a server at 3003

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3003
+
+const server = app.listen(port, () =>
+	console.log("Server Listening on " + port)
+)
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.status(200).send("This is the home Page")
+})
