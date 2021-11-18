4a912064bbf83e9181a31d0a753cab5801dd2f29
tharunoptimus <tharunoptimus@outlook.com>
2021-11-18 15:03:45 +0530

    Imported loggedIn middleware to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3003
+const middleware = require("./middleware")
 const mongoose = require("./database")
 
 app.listen(port, () =>
