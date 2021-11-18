3fb91f9ab30e2570fcfe818932eb859c2bde8221
tharunoptimus <tharunoptimus@outlook.com>
2021-11-18 18:50:12 +0530

    Added get to drawingRoutes

--- routes/drawingRoutes.js
+++ routes/drawingRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const app = express()
 const router = express.Router()
-const bcrypt = require("bcrypt")
 const User = require("../schemas/UserSchema")
+const Paint = require("../schemas/PaintSchema")
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -11,5 +11,20 @@ app.set("view engine", "pug")
 app.set("views", "views")
 
 router.get("/", (_, res) => res.redirect("/"))
+
+router.get("/:id", async (req, res) => {
+	const { id } = req.params
+	let paint = await Paint.findById(id)
+
+	if (paint == null) return res.redirect("/")
+	if (!paint.isShared) return res.redirect("/")
+
+	let payload = {
+		pageTitle: paint.title,
+		paint: JSON.stringify(paint),
+	}
+
+	return res.status(200).render("paint", payload)
+})
 
 module.exports = router
