d3fcf52c2127ad1059779301742b6a2adebd58c8
tharunoptimus <tharunoptimus@outlook.com>
2021-11-19 14:27:41 +0530

    Can click to edit the paint from home

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,11 @@
-let createNewPaintDiv = document.getElementById('newPaint')
-createNewPaintDiv.addEventListener('click', e => {
-    return window.location.href = '/paint/new/create'
+let createNewPaintDiv = document.getElementById("newPaint")
+createNewPaintDiv.addEventListener("click", (e) => {
+	return (window.location.href = "/paint/new/create")
+})
+
+document.querySelectorAll(".paintContainer").forEach((item) => {
+	item.addEventListener("click", (event) => {
+		let id = event.target.getAttribute("data-url")
+		window.location.href = `paint/${id}`
+	})
 })
