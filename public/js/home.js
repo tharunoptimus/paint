c615ce5fee1bed09610dd061147affed7912a52c
tharunoptimus <tharunoptimus@outlook.com>
2021-11-19 21:58:21 +0530

    Added sw to home

--- public/js/home.js
+++ public/js/home.js
@@ -58,3 +58,11 @@ function addEventListenerToPaintContainer () {
 		})
 	})
 }
+
+if('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/sw.js')
+			.then((reg) => console.log('Success: ', reg.scope))
+			.catch((err) => console.log('Failure: ', err));
+	})
+}
