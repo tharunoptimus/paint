3bb91a317b493edf5219635e6c4087ce17fb7f17
tharunoptimus <tharunoptimus@outlook.com>
2021-11-19 12:32:42 +0530

    Added new api route to edit title

--- routes/api/paint.js
+++ routes/api/paint.js
@@ -9,6 +9,21 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
 
+router.post("/edit/title", async (req, res) => {
+    let { paintId, title, userId } = req.params
+
+    let user = await User.findById(user)
+    if(!user.paints.includes(paintId)) return res.status(401).send("You are not authorized to edit this")
+    
+    try {
+        let paint = await Paint.findByIdAndUpdate(paintId, { title: title})
+        return res.status(204).send(title)
+    } catch (err) {
+        console.log(err)
+        return res.status(400).send("Unable to Edit. Please Try again!")
+    }
+    
+})
 
 
 module.exports = router;
